a56b6b9a5683ab5b6eb70ebb043d5c8e7e4159f7
danoux10 <danybarbe10@gmail.com>
2021-04-08 15:48:29 +0200

    fichier table réaliser

---
+++ _classes/tableau_ruche.php
@@ -0,0 +1,49 @@
+<?php
+    $ruche_selectionner = $bdd_ruche->query('SELECT * FROM ruche_data WHERE ruche_id='.$_SESSION['ruche'].' ORDER BY jour ASC');
+    echo "<div class='table'>";
+     echo "<table class='affiche'>";
+                echo "<thead class='table_head'>";
+                    echo "<tr class='cellule'>";
+                        echo "<th colspan='2' class='centre'>id</th>";
+                        echo "<th colspan='2' class='centre'>date</th>";
+                        echo "<th colspan='2' class='centre'>heure</th>";
+                        echo "<th colspan='2' class='centre'>poids</th>";
+                        echo "<th colspan='2' class='centre'>temperature</th>";
+                        echo "<th colspan='2' class='centre'>humidité</th>";
+                    echo "</tr>";
+                echo "</thead>";
+                echo "<tbody>";
+                
+                while($donner = $ruche_selectionner->fetch()){
+                    
+                    //variable
+                    $id = $donner['ruche_id'];                
+                    $poids = $donner['poids'];
+                    $temperature = $donner['temperature'];
+                    $humiditer = $donner['humidite'];
+                    
+                    //formatage date/heure
+                    $datetime = $donner['jour'];
+                    
+                    $annee = substr($datetime, 0,4);
+                    $mois = substr($datetime, 5,2);
+                    $jours = substr($datetime, 8,2);
+                    
+                    $heure = substr($datetime,11,8);
+                    
+                    $date = $jours."/".$mois."/".$annee;
+                    
+                    //affichage
+                    echo "<tr class='cellule'>";
+                        echo "<td colspan='2' class='centre'>$id</td>";
+                        echo "<td colspan='2' class='centre'>$date</td>";
+                        echo "<td colspan='2' class='centre'>$heure</td>";
+                        echo "<td colspan='2' class='centre'>$poids Kg</td>";
+                        echo "<td colspan='2' class='centre'>$temperature °C</td>";
+                        echo "<td colspan='2' class='centre'>$humiditer %</td>";
+                    echo "</tr>";
+                }
+                echo "</tbody>";
+            echo "</table>";
+        echo "</div>";
+?>
---
+++ _classes/tableau_utilisateur.php
@@ -0,0 +1,29 @@
+<?php
+    $utilisateur = $bdd_utilisateur->query('SELECT * FROM utilisateur ORDER BY id');
+     echo "<table class='affiche_utilisateur'>";
+                echo "<thead>";
+                    echo "<tr class='cellule table_head'>";
+                        echo "<th colspan='2' class='centre'>nom</th>";
+                        echo "<th colspan='2' class='centre'>prenom</th>";
+                        echo "<th colspan='2' class='centre'>email</th>";
+                    echo "</tr>";
+                echo "</thead>";
+                echo "<tbody>";
+                
+                while($affichage = $utilisateur->fetch()){
+                    
+                    //variable
+                    $nom = $affichage['nom'];
+                    $prenom = $affichage['prenom'];
+                    $email = $affichage['email'];
+                    
+                    //affichage
+                    echo "<tr class='cellule'>"; 
+                        echo "<td colspan='2' class='centre'>$nom</td>";
+                        echo "<td colspan='2' class='centre'>$prenom</td>";
+                        echo "<td colspan='2' class='centre'>$email</td>";
+                    echo "</tr>";
+                }
+                echo "</tbody>";
+	  echo "</table>";
+?>
