df027b4ab0e876bec5663133be782a3c767e9060
danoux10 <danybarbe10@gmail.com>
2021-04-11 22:42:12 +0200

    inscription ajout de class

--- views/inscription.php
+++ views/inscription.php
@@ -13,38 +13,38 @@
 				<img src="../img/honey.svg" alt="pot_de_miel" id="image_miel_inscription">
 			</div>
 			
-			<div id="nom_prenom_inscription">
+			<div id="nom_prenom_inscription" class="inscription_partie">
 				<div id="nom_inscription">
-					<label id="inscription_nom_label">Nom</label>
+					<label id="inscription_nom_label" class="inscription_label">Nom</label>
 					<input type="text" id="inscription_nom_input" class="inscription_nom_prenom" name="nom">
 				</div>
 				
 				<div id="prenom_inscription">
-					<label id="inscription_prenom_label">Prenom</label>
+					<label id="inscription_prenom_label" class="inscription_label">Prenom</label>
 					<input type="text" id="inscription_prenom_input" class="inscription_nom_prenom" name="prenom">
 				</div>
 			</div>
 			
-			<div id="email_emailconfirm_inscription">
+			<div id="email_emailconfirm_inscription" class="inscription_partie">
 				<div id="email_inscription">
-					<label id="inscription_email_label">Email</label>
+					<label id="inscription_email_label" class="inscription_label">Email</label>
 					<input type="text" id="inscription_email_input" class="inscription_email" name="email">
 				</div>
 				
 				<div id="confirm_email_inscription">
-					<label id="inscription_confirm_email_label">Confirm Email</label>
-					<input type="text" id="inscription_confirm_email_label" class="inscription_email" name="confirm_email">
+					<label id="inscription_confirm_email_label" class="inscription_label">Confirm Email</label>
+					<input type="text" id="inscription_confirm_email_input" class="inscription_email" name="confirm_email">
 				</div>
 			</div>
 			
-			<div id="password_confirm_password_inscription">
+			<div id="password_confirm_password_inscription" class="inscription_partie">
 				<div id="inscription_password">
-					<label id="inscription_password_label">Password</label>
+					<label id="inscription_password_label" class="inscription_label">Password</label>
 					<input type="password" id="inscription_password_label" class="inscription_password" name="password">
 				</div>
 				
 				<div id="inscription_password_confirm">
-					<label id="inscription_password_confirm_label">Confirm Password</label>
+					<label id="inscription_password_confirm_label" class="inscription_label">Confirm Password</label>
 					<input type="password" id="inscription_password_confirm_input" class="inscription_password" name="confirm_password">
 				</div>
 			</div>
