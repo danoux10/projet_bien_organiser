ff85cb54b9cef8e45444a90447014f9d090135a3
danoux10 <danybarbe10@gmail.com>
2021-04-14 14:11:29 +0200

    code ruche bon normalement reste a test

--- views/ruche.php
+++ views/ruche.php
@@ -4,73 +4,64 @@
 <title>Observer Ruche</title>
 </head>
 <body>
-	 <?php
+	<?php
         if(isset($_POST['valide_ruche'])){
             $_SESSION['ruche'] = $_POST['ruche'];
             $ruche=$_POST['ruche'];
-            echo $ruche;
-        }              
-        
-        if(isset($_POST['valide_date'])){
-            $_SESSION['date'] = $_POST['date'];
-            $date = $_POST['date'];
-            echo $date;
         }
     ?>
-    
-    <div>
-        <!--form selection ruche-->
-        <form method="post">
-            <?php 
-                print $select_ruche; 
-            ?>
-            <input type="submit" value="valider ruche" name="valide_ruche">
-        </form>
-        
-        <!--form selection date-->
-        <!--<form method="post">
-            <?php 
-                //print $select_date;
-            ?>
-            <input type="submit" value="valider date" name="valide_date">
-        </form>-->
-        
-        <!--form csv-->
-        <form method="post" action="csv.php">
-            <input type="submit" value="fichier csv" name="csv">        
-        </form>
-    
-        <!--form pdf-->
-        <form method="post" action="pdf.php">
-            <input type="submit" value="fichier pdf" name="pdf">        
-        </form>
-        
-        <!--form affichage tableau/graphique-->
-        <form method="post">
-            <input type="submit" value="afficher tableau" name="tableau">
-            <input type="submit" value="afficher graphique" name="graphique">
-            <input type="submit" value="afficher graphique et tableau" name="graph_table">
-        </form>
-        
-       <!-- <form method="post">
-            <input type="submit" value="choisir date" name="date">
-        </form>-->
-        
-    </div>
+    <div class="bloc_nav_ruche">
+        <div id="titre">
+            <h2 class="titre_ruche_observation">Observer ruche</h2>
 
-    <?php     
-        if(isset($_POST['graphique'])){
-            include '../_classes/graphique.php';
-        }
+            <!--titre ruche-->
+            <?php 
+                echo "<h3 class='titre_ruche'>ruche n°".$_SESSION['ruche']."</h3>";
+            ?>
+        </div>
+    
+        <div class="formulaire">
+
+            <!--form selection ruche-->
+            <form method="post" class="selection_ruche">
+                <?php 
+                    print $select_ruche; 
+                ?>
+                <input type="submit" value="valider ruche" name="valide_ruche">
+            </form>
         
-        if(isset($_POST['tableau'])){
-            include '../_classes/tableau.php';
-        }
+            <div class="csv_pdf">
+                <table class="table_csv_pdf">
+                    <tr class="cellule_csv_pdf">
+                        <td>Télécharger les données :</td>
+                        <td> <a href="csv.php" class="lien_csv_pdf">excel</a> </td>
+                        <td> / </td>
+                        <td> <a href="pdf.php" class="lien_csv_pdf">pdf</a> </td>
+                    </tr>
+                </table>
+            </div>
         
-        if(isset($_POST['graph_table'])){
-            include '../_classes/graphique.php';
-            include '../_classes/tableau.php';
-        }
-    ?>
+            <!--form affichage tableau/graphique-->
+            <table id="table_graph">
+                <tr class="cellule_graph">
+                        <td>Afficher :</td>
+                    <form method="post" class="tableau_graph">
+                        <td> <input type="submit" value="tableau" name="tableau" class="bt_graph"> </td>
+                    </form>
+                </tr>
+            </table>
+        </div>
+    </div>
+    
+    <div id="tableau_graph">
+        <?php        
+        
+            if(isset($_POST['tableau'])){
+                include '../_function/tableau_ruche.php';
+            }
+        
+            include '../_function/graphique.php';  
+        ?>
+    </div>
 </body>
 </html>
