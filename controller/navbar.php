760b4f54064a2f75685caea82bc7a3e1e1ef58dc
danoux10 <danybarbe10@gmail.com>
2021-04-14 15:40:56 +0200

    navbar mais pas le php qui vas avec

---
+++ controller/navbar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<header id="navbar">
+    <nav class="navbar_nav">
+      <ul class="nav">
+        <a href="#" class="li_a"> <li class="nav_li">Acceuil</li> </a>
+        <a href="#" class="li_a"> <li class="nav_li suite">Donner Ruche</li> </a>
+        <a href="#" class="li_a"> <li class="nav_li suite">administre utilisateur</li> </a>    
+        <a href="#" class="li_a"> <li class="nav_li suite">se deconnecter</li> </a>
+        <a href="connection.php" class="li_a"> <li class="nav_li suite">se connecter</li> </a>
+      </ul>
+    </nav> 
+</body>
+</html>
